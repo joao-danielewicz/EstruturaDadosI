6819c06714910336cc41b195f686bb12d00f61eb
João Danielewicz <joao.wicz@gmail.com>
2023-10-10 16:02:59 -0300

    commit

---
+++ Trabalhos/BubbleSort/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] valores = new int[] {6,7,8,4,10};
+int aux = 0;
+
+foreach(int valor in valores){
+        Console.Write($"{valor} | ");
+    }
+Console.WriteLine();
+
+for(int i=0; i<(valores.GetLength(0)-1); i++){
+    for(int j=0; j<(valores.GetLength(0)-1); j++){
+        if(valores[j]>valores[j+1]){
+            aux = valores[j];
+            valores[j] = valores[j+1];
+            valores[j+1] = aux;            
+        
+        }
+    }
+    foreach(int valor in valores){
+        Console.Write($"{valor} | ");
+    }
+    Console.WriteLine();
+}
+
+foreach(int valor in valores){
+    Console.Write($"{valor} | ");
+}
