75b24e4032721da8e3a09c50df8bb24f2a570e46
João Danielewicz <joao.wicz@gmail.com>
2023-10-11 15:36:39 -0300

    commit

--- Trabalhos/BubbleSort/Program.cs
+++ Trabalhos/BubbleSort/Program.cs
@@ -8,34 +8,43 @@ foreach(int valor in valores){
     }
 Console.WriteLine("\n");
 
-for(int i=0; i<(valores.GetLength(0)-1); i++){
+// Loop exterior: usado para percorrer todos os itens do vetor e flutuá-los até o topo.
+// Loop interior: usado para comparar o elemento da vez com o seu superior, até que este ocupe a maior posição possível.
+
+// Condicional: se o item sendo comparado for menor que aquele que está acima, nada é feito. Caso contrário,
+// ele será guardado numa variável auxiliar, seu espaço original será ocupado por seu superior (que é menor),
+// e ele passará a ocupar a posição original + 1 (que antes pertencia ao numero menor).
+
+for(int i=0; i<(valores.GetLength(0)); i++){
     for(int j=0; j<(valores.GetLength(0)-1); j++){
         if(valores[j]>valores[j+1]){
             aux = valores[j];
             valores[j] = valores[j+1];
             valores[j+1] = aux;            
 
-            Console.Write($"Iteração {i+1}:               ");
+            Console.Write($"Iteração {i+1}:               | ");
             foreach(int valor in valores){
                 Console.Write($"{valor} | ");
             }
             Console.WriteLine();    
         }
     }
-    Console.Write($"Iteração {i+1} completa:      ");
+    Console.Write($"Iteração {i+1} completa:      | ");
     foreach(int valor in valores){
         Console.Write($"{valor} | ");
     }
     Console.WriteLine("\n");
 }
 
-Console.Write("Matriz ordenada:          ");
+Console.Write("Matriz ordenada:          | ");
 foreach(int valor in valores){
     Console.Write($"{valor} | ");
 }
 
 
-
+// O algoritmo de ordenação por bolha é simples. Abaixo, está apresentado sem nenhum método de impressão, e ocupa apenas onze linhas.
+// int[] valores = new int[];
+// int aux;
 // for(int i=0; i<(valores.GetLength(0)-1); i++){
 //     for(int j=0; j<(valores.GetLength(0)-1); j++){
 //         if(valores[j]>valores[j+1]){
