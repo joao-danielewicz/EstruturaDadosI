1b78cf25eba4f63fe39b87b8cd51f2e58a2dcb63
João Danielewicz <joao.wicz@gmail.com>
2023-10-11 11:12:52 -0300

    commit

--- Trabalhos/BubbleSort/Program.cs
+++ Trabalhos/BubbleSort/Program.cs
@@ -1,10 +1,12 @@
-﻿int[] valores = new int[] {6,7,8,4,10};
-int aux = 0;
+﻿Console.Clear();
+int[] valores = new int[] {5,1,4,2,8};
+int aux;
 
+Console.Write("Matriz desordenada:       ");
 foreach(int valor in valores){
         Console.Write($"{valor} | ");
     }
-Console.WriteLine();
+Console.WriteLine("\n");
 
 for(int i=0; i<(valores.GetLength(0)-1); i++){
     for(int j=0; j<(valores.GetLength(0)-1); j++){
@@ -12,15 +14,34 @@ for(int i=0; i<(valores.GetLength(0)-1); i++){
             aux = valores[j];
             valores[j] = valores[j+1];
             valores[j+1] = aux;            
-        
+
+            Console.Write($"Iteração {i+1}:               ");
+            foreach(int valor in valores){
+                Console.Write($"{valor} | ");
+            }
+            Console.WriteLine();    
         }
     }
+    Console.Write($"Iteração {i+1} completa:      ");
     foreach(int valor in valores){
         Console.Write($"{valor} | ");
     }
-    Console.WriteLine();
+    Console.WriteLine("\n");
 }
 
+Console.Write("Matriz ordenada:          ");
 foreach(int valor in valores){
     Console.Write($"{valor} | ");
 }
+
+
+
+// for(int i=0; i<(valores.GetLength(0)-1); i++){
+//     for(int j=0; j<(valores.GetLength(0)-1); j++){
+//         if(valores[j]>valores[j+1]){
+//             aux = valores[j];
+//             valores[j] = valores[j+1];
+//             valores[j+1] = aux;            
+//         }
+//     }
+// }
