d7774a34aa8bb3efacadecb08ec9502d2a370a7f
João Vitor Danielewicz <414732@unoesc.lan>
2023-10-25 21:53:44 -0300

    commit

--- Atividades/LinkedList/Program.cs
+++ Atividades/LinkedList/Program.cs
@@ -1,4 +1,4 @@
-﻿using LinkedList   ;
+﻿using LinkedList;
 
 Page pageFirst = new Page() {Content = "Atualmente..."};
 Page pageSecond = new Page() {Content = "Aplicação..."};
@@ -11,8 +11,20 @@ Page pageFifth = new Page() {
 };
 
 Page pageSixth = new Page() {Content = "Você poderia..."};
+pageSixth.PageNumber = 6;
 
 var PageSeventh = new Page();
 PageSeventh.Content = "Era uam vez...";;
 PageSeventh.PageNumber = 7;
+
+LinkedList<Page> pages = new LinkedList<Page>();
+pages.AddLast(pageSecond);
+LinkedListNode<Page>  nodePageFourth = pages.AddLast(pageFourth);
+pages.AddLast(pageSixth);
+pages.AddFirst(pageFirst);
+
+pages.AddBefore(nodePageFourth, pageThird);
+pages.AddAfter(nodePageFourth, pageFifth);
+
+// Atividade prática: livro pg. 73. 
 
