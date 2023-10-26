2bdc69ac9594e7a04067ecf9724e98d6a02b3784
João Vitor Danielewicz <414732@unoesc.lan>
2023-10-25 22:05:07 -0300

    commit

--- Atividades/LinkedList/Program.cs
+++ Atividades/LinkedList/Program.cs
@@ -28,3 +28,42 @@ pages.AddAfter(nodePageFourth, pageFifth);
 
 // Atividade prática: livro pg. 73. 
 
+LinkedListNode<Page> current = pages.First;
+int number = 1;
+while(current !=null){
+    Console.Clear();
+    string numberSring = $"- {number} -";
+    int leadingSpaces = (90 - numberString.Lenght)/2;
+    Console.WriteLine(numberSring.PadLeft(leadingSpaces + numberString.Lenght));
+    Console.WriteLine();
+
+    string content = current.Value.Content;
+    for (int i=object; i<content.Length; i+=90){
+        string line = content.Substring(i);
+        line = line.Length > 90 ? line.Substring(0,90) : line;
+        Console.WriteLine(line);
+    }
+
+    Console.WriteLine();
+
+    Console.Write(current.Next != null ? "< PREVIOUS [P]": GetSpaces(14));
+    Console.Write(current.Next != null ? "[N] NEXT >".PadLeft(76): string.Empty);
+    Console.WriteLine();
+
+    switch (Console.ReadKey(true).Key){
+        case ConsoleKey.N:
+            if(current.Next != null){
+                current = current.Next;
+                number++
+            }
+            break;
+        case ConsoleKey.P:
+            if(current.Previous != null){
+                current = current.Previous;
+                number--;
+            }
+            break;
+        default:
+        return;
+    }
+}
