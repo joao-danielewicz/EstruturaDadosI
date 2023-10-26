d70741e65e6e0679a6261451aa795f5b1fd68e98
João Vitor Danielewicz <414732@unoesc.lan>
2023-10-25 21:25:25 -0300

    commit

---
+++ Atividades/LinkedList/Page.cs
@@ -0,0 +1,8 @@
+namespace LinkedList{
+    public class Page{
+        public string? Content {get; set;}
+
+        public string? PageNumber {get; set;}
+        
+    }
+}
---
+++ Atividades/LinkedList/Program.cs
@@ -0,0 +1,18 @@
+﻿using LinkedList   ;
+
+Page pageFirst = new Page() {Content = "Atualmente..."};
+Page pageSecond = new Page() {Content = "Aplicação..."};
+Page pageThird = new Page() {Content = "Muito de..."};
+Page pageFourth = new Page() {Content = "Você sabia...", PageNumber = 4};
+
+Page pageFifth = new Page() {
+    Content = "Enquanto...",
+    PageNumber = 5;
+};
+
+Page pageSixth = new Page() {Content = "Você poderia..."};
+
+var PageSeventh = new Page();
+PageSeventh.Content = "Era uam vez...";;
+PageSeventh.PageNumber = 7;
+
